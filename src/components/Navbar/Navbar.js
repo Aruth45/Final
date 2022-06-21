e62f02c08d781244461e74d6787674d61715600a
unknown <danielmoracubero@gmail.com>
2022-06-20 23:48:18 -0500

    initial version

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  const block = "navbar";
+  const [menu, setMenu] = useState(false);
+
+  return (
+    <header className="header">
+      <nav
+        aria-label="Mobile navigation main menu"
+        role="navigation"
+        className={`${block}`}
+      >
+        <NavLink
+          aria-label="Go to home page"
+          className={`${block}__logo`}
+          to="/"
+        >
+          <i className="fa fa-solid fa-book"></i>
+        </NavLink>
+        <button
+          title={menu ? "Close main menu" : "Open main menu"}
+          aria-haspopup="menu"
+          aria-label={menu ? "Close main menu" : "Open main menu"}
+          aria-expanded={menu ? true : false}
+          onClick={() => setMenu(!menu)}
+          className={`${block}__btn`}
+        >
+          <div
+            className={
+              menu
+                ? `${block}__burger ${block}__burger--open`
+                : `${block}__burger`
+            }
+          ></div>
+        </button>
+        <ul role="menu" className={`${block}__list`}>
+          <li role="presentation" className={`${block}__item`}>
+            <NavLink role="menuitem" to="/">
+              Hola
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
